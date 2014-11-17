18bcf531e12e002470589f2be546c8b96b64fc44
Anthony Picciano <anthony.picciano@gmail.com>
2014-11-17 16:26:02 -0700

    Initial Commit

---
+++ MechArm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MechArm
+//
+//  Created by Anthony Picciano on 11/17/14.
+//  Copyright (c) 2014 Crispin Porter + Bogusky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
